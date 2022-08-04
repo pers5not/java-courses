32674bbf8d3398eeab605d0c0cb94e6adca28444
pers5not <pers5not@gmail.com>
2022-08-04 20:08:52 +0300

    add comparing three numbers Java

---
+++ first-week-tasks/java-scaner/Nums.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Nums here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Nums
+{
+        public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner =new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if ( a == c && b == c)
+            System.out.printf("%s %s %s", a, a, a);
+        else if (a == b || a == c)
+            System.out.printf("%s %s", a, a);
+        else if (b == c)
+            System.out.printf("%s %s", b, b);
+
+    }
+
+}
