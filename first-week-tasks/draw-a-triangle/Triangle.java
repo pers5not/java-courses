19df2859e71e6539b81bc812ce8b902f691e89b1
pers5not <pers5not@gmail.com>
2022-08-05 19:12:12 +0300

    draw right triangle loop for java

---
+++ first-week-tasks/draw-a-triangle/Triangle.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Triangle here.
+ * 
+ * @author pers5not
+ * @version 05/08/2022
+ */
+public class Triangle
+{
+    public static void Main (String[] args){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j <= i; j++)
+                System.out.print("*"); 
+        System.out.println();
+   }
+    }
+   
+}
