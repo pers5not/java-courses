d72d57a5b69cbe733f834ff60189ae21d41996d5
pers5not <pers5not@gmail.com>
2022-08-07 16:45:26 +0300

    add class Arrays method toString

---
+++ first-week-tasks/MyProject/src/Solution/ArraysToString.java
@@ -0,0 +1,11 @@
+package Solution;
+
+import java.util.Arrays;
+
+public class ArraysToString {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        String str = Arrays.toString(array);
+        System.out.println(str);
+    }
+}
