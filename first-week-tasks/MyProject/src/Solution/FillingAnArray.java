721907b213b5887c6a6b2c42cd657a7258f7a1e6
pers5not <pers5not@gmail.com>
2022-08-07 19:57:42 +0300

    add Java class build jagged array

--- first-week-tasks/MyProject/src/Solution/FillingAnArray.java
+++ first-week-tasks/MyProject/src/Solution/FillingAnArray.java
@@ -10,14 +10,16 @@ public class FillingAnArray {
     public static void main(String[] args) {
         double num = Math.ceil(array.length / 2.0);
         if (array.length % 2 == 0) {
-            Arrays.fill(array, 0, (int) num, valueStart);
-            Arrays.fill(array, (int) num, array.length, valueEnd);
+            Arrays.fill(array,0, (int)num, valueStart);
+            Arrays.fill(array,(int)num, array.length, valueEnd);
             String str = Arrays.toString(array);
             System.out.println(str);
         } else {
-            Arrays.fill(array, 0, (int) num, valueStart);
-            Arrays.fill(array, (int) num, array.length, valueEnd);
+            Arrays.fill(array,0, (int)num, valueStart);
+            Arrays.fill(array,(int)num, array.length, valueEnd);
             String str = Arrays.toString(array);
+            System.out.println(str);
+
         }
 
     }
