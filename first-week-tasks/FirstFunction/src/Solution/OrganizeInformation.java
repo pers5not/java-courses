64e4aca6f47393e611c1d317441919e4935d6f44
pers5not <pers5not@gmail.com>
2022-08-07 22:54:39 +0300

    add method print text

---
+++ first-week-tasks/FirstFunction/src/Solution/OrganizeInformation.java
@@ -0,0 +1,13 @@
+package Solution;
+
+public class OrganizeInformation {
+    public static void printText() {
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        System.out.println("От меня вам всем приветик.");
+
+    }
+
+    public static void main(String[] args) {
+        printText();
+    }
+}
